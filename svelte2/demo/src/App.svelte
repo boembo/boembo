653c1a15ae269bed43fa02075134daa7fdce3c68
LAPTOP-K4NBCK1T\linh <l.linh@softsu.co.jp>
2024-06-11 15:02:59 +0900

    Dynamic Load Component based on vars

--- svelte2/demo/src/App.svelte
+++ svelte2/demo/src/App.svelte
@@ -1,6 +1,21 @@
 <script>
-  import Board from './components/Board.svelte';
-  import { tasks } from './stores/tasks';
+import Error404 from './components/Error404.svelte';
+import { tasks } from './stores/tasks';
+ import { onMount } from 'svelte'
+
+  let dynamicPage = null
+   let component = "Board"
+
+  onMount(async () => {
+      try {
+          dynamicPage = (await import(`./components/${component}.svelte`)).default
+      } catch (e) {
+console.log("Error");
+          // Handle errors if the dynamic route doesn't load:
+          dynamicPage = Error404
+      }
+  })
 </script>
 
-<Board />
+
+<svelte:component this={dynamicPage} />
---
+++ svelte2/demo/src/components/Error404.svelte
@@ -0,0 +1,3 @@
+<li class="p-2 bg-blue-100 rounded shadow" >
+  404
+</li>
